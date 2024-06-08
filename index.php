e7bbc18dcf6ec37924f2bd751fe4fdb5916b3bb8
Edson Baierle <edsonbaierle@gmail.com>
2024-06-08 10:55:55 -0300

    Montagem Estrutura

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
+    require_once __DIR__ . "/vendor/autoload.php";
+    header('Acces-Control-Allow-Origin: *');
+    header('Content-type: application/json');
+    date_default_timezone_set('America/Sao_Paulo');
 
-    echo "ola";
+    use App\Core\Core;
+    require_once __DIR__ . "/src/Routes/routes.php";
+    
+    $core = new Core();
+    $core->run($routes);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'fcae50262f7af194fd9f9535ece64a36776a182f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'fcae50262f7af194fd9f9535ece64a36776a182f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
